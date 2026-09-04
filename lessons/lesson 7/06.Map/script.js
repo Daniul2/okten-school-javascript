5ffb6d4185f805defa47691df5c3c6352c1432ea
Daniil Medvediev <medvedievdaniil300@gmail.com>
2026-09-04 15:26:46 +0200

    lesson 7

---
+++ lessons/lesson 7/06.Map/script.js
@@ -0,0 +1,23 @@
+let map = new Map();
+console.log(map);
+
+let likar = {id: 'likar'};
+let petya = {id: 'petya'};
+
+
+map.set(likar, {name: 'roma', surname: 'oganov'});
+map.set(petya, {name: 'oleksandr', surname: 'petrov'});
+map.set(petya, {name: 'asdqwe', surname: 'qweqwe'});
+
+console.log(map);
+
+console.log(map.get(likar));
+console.log(map.size);
+console.log(map.keys());
+
+let from = Array.from(map.keys());
+console.log(from);
+
+console.log(map.values());
+
+console.log(Array.from(map.values()));
